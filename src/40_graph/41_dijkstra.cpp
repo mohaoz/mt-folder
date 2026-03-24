3f73068cb0da00bfebe7f97129007aa32bc24933
mohao <31468168+mohaoz@users.noreply.github.com>
2026-03-24 21:28:19 +0800

    chore

---
+++ src/40_graph/41_dijkstra.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int INF = 1e9;
+
+vector<int> dijkstra(const auto &adj, int n, int s) {
+    vector<int> dis(n + 1, INF);
+    vector<bool> vis(n + 1, false);
+    priority_queue<pair<int, int>> pq;
+    dis[s] = 0;
+    pq.emplace(0, s);
+    while (!pq.empty()) {
+        auto [_, u] = pq.top(); pq.pop();
+        if (vis[u]) 
+            continue;
+        vis[u] = true;
+        for (auto [v, w] : adj[u]) {
+            if (dis[u] + w < dis[v]) {
+                dis[v] = dis[u] + w;
+                pq.emplace(-dis[v], v);
+            }
+        }
+    }
+    return dis;
+}
+
+void P4779() {
+    int n, m, s;
+    cin >> n >> m >> s;
+    vector adj(n + 1, vector<pair<int, int>>{});
+    while (m--) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].emplace_back(v, w);
+    }
+    auto dis = dijkstra(adj, n, s);
+    for (int i = 1; i <= n; ++i) {
+        cout << dis[i] << " \n"[i == n];
+    }
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    P4779();
+}
